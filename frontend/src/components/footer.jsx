16df2bec66349508ac9848dcca830db9fef7d49b
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2025-02-17 09:26:50 -0500

    footer work

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -6,7 +6,12 @@ function Footer() {
     return (
         <footer className="footer">
             <img src="/plant_hall.png" alt="Plant Hall img" className="logo" />  {/* Plant Hall Logo */}
-
+            <div className="social-links">
+                <h2>Social Media</h2>
+                <a href="https://www.instagram.com/uoftampa/" target="_blank" rel="noopener noreferrer">
+                    <img src="/instagram.svg" alt="Instagram" />
+                </a>
+            </div>
             <div className="footer-items">
                 <h2>Contact Us</h2>
                 <h2>FAQs</h2>
