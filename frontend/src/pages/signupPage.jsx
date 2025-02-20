5388d7f50515b0dfa94f17808efe6782c0855252
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2025-02-19 23:15:07 -0500

    updated signup page

--- frontend/src/pages/signupPage.jsx
+++ frontend/src/pages/signupPage.jsx
@@ -6,20 +6,36 @@ import './signupPage.css'
 function Signup() {
 
     // update the text field when user puts in email and password
-    const [formData, setFormData] = useState({name: "", email: "", password: "" });
+    const [formData, setFormData] =
+        useState({name: "", primaryEmail: "", password: "" });
+
+    // check and handle the changes in the input fields
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    // allow for redirection after singup is complete
     const navigate = useNavigate();
 
-
+    //  need to finish handleSubmit
+    // add a try, catch to fetch the backend api and update it by creating a new user
+    // IF signup succesful nviage to another page (logged in), or throw and error and retry signup
+    //const handleSubmit = async (e) => {
+    //    e.preventDefault();
 
 
 
     return (
-        <div className="signupPage">
-            <h2>Signup</h2>
-            <form action="">
-            </form>
-
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input type="text" name="name" placeholder="Name" value={formData.username} onChange={handleChange} required />
+                <input type="email" name="email" placeholder="Email" value={formData.primaryEmail} onChange={handleChange} required />
+                <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
+            </div>
+            <div>
+                <button type="submit">Sign Up</button>
+            </div>
+        </form>
     )
 }
 export default Signup;
