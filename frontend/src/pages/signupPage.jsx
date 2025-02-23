4bf77895c5d14463b1138be7a5dd2f28f684fea9
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2025-02-23 18:14:25 -0500

    signup sending the results to backend

--- frontend/src/pages/signupPage.jsx
+++ frontend/src/pages/signupPage.jsx
@@ -6,7 +6,7 @@ function Signup() {
 
     // update the text field when user puts in email and password
     const [formData, setFormData] =
-        useState({name: "", primaryEmail: "", password: "", verifyPassword: ""});
+        useState({name: "", email: "", password: "", verifyPassword: ""});
 
     // check and handle the changes in the input fields
     const handleChange = (e) => {
@@ -21,14 +21,41 @@ function Signup() {
     // IF signup succesful nviage to another page (logged in), or throw and error and retry signup
     const handleSubmit = async (e) => {
             e.preventDefault();
-    }
+
+            if (formData.password !== formData.verifyPassword) {
+                alert("Passwords don't match");
+                return;
+            }
+
+            try {
+                // attempt to send to backend as body and wait for its response
+                const response = await fetch ("http://localhost:8000/signup", {
+                    method: "POST",  // send post request and create a new user
+                    headers: { "Content-Type": "application/json" },  // the type of data is json since we use mongoDB
+                    body: JSON.stringify({
+                        name: formData.name,
+                        email: formData.email,
+                        password: formData.password,
+                    }),
+                });
+
+                if (response.ok) {
+                    navigate("/");
+                } else {
+                    const errorData = await response.json();
+                    alert(errorData.message || "Signup failed. Please try again.");
+                }
+            } catch (error) {
+                alert(error);
+            }
+    };
 
 
     return (
         <form onSubmit={handleSubmit}>
             <>
                 <input type="text" name="name" placeholder="Name" value={formData.name} onChange={handleChange} required />
-                <input type="email" name="primaryEmail" placeholder="Email" value={formData.primaryEmail} onChange={handleChange} required />
+                <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
                 <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
                 <input type="password" name="verifyPassword" placeholder="Verify Password" value={formData.verifyPassword} onChange={handleChange} required />
             </>
