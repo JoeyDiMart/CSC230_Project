44885ad962419b35629982a93a31928fee710f40
stamayoz <138267058+stamayoz@users.noreply.github.com>
2025-02-25 10:33:35 -0500

    Updated Navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,17 +1,31 @@
 import { Link, useNavigate } from "react-router-dom";
 import React from "react";
+import { useState } from "react";
 import "./navbar.css";
 
 function Navbar() {
-    const navigate = useNavigate();
+    const navigate = useNavigate();  
+
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+
+
+
     return (
         <nav className="navbar">
             <div className="nav-items">
-                <ul>
-                    <li><Link to="/Publications">Publications</Link></li>
-                    <li><Link to="/Events">Events</Link></li>
-                    <li><Link to="/About">About Us</Link></li>
-                    <li><Link to="/Associates">Research Associates</Link></li>
+
+                <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times rotate' : 'fas fa-bars'}/>
+                </div>
+
+                {/* Conditional to activate the nav menu*/}
+                <ul className={click? 'nav-menu active' : 'nav-menu'}>
+                    <li><Link to="/Publications" onClick={closeMobileMenu}>Publications</Link></li>
+                    <li><Link to="/Events" onClick={closeMobileMenu}>Events</Link></li>
+                    <li><Link to="/About" onClick={closeMobileMenu}>About Us</Link></li>
+                    <li><Link to="/Associates" onClick={closeMobileMenu}>Research Associates</Link></li>
                 </ul>
             </div>
 
