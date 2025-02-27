004ad63f29d015f0c0e1767e789c29015a44f546
tdesiderio <trevor.desiderio@spartans.ut.edu>
2025-02-27 10:02:14 -0500

    Footer updated and links to social media

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -5,21 +5,30 @@ import "./footer.css";
 function Footer() {
     return (
         <footer className="footer">
-            <img src="/plant_hall.png" alt="Plant Hall img" className="logo" />  {/* Plant Hall Logo */}
-            <div className="social-links">
-                <h3>Social Media</h3>
-                <a href="https://www.instagram.com/uoftampa/" target="_blank" rel="noopener noreferrer">
-                    <img src="/instagram.svg" alt="Instagram" />
-                </a>
-                <a href="https://www.tiktok.com/@uoftampa" target="_blank" rel="noopener noreferrer">
-                    <img src="/tik_tok.svg" alt="Tik Tok" />
-                </a>
-
-            </div>
+            <a href="https://www.ut.edu/" target="_blank" rel="noopener noreferrer">
+                <img src="/plant_hall.png" alt="Plant Hall" />
+            </a>
             <div className="footer-items">
                 <h3>Contact Us</h3>
                 <h3>FAQs</h3>
                 <h3>Resources</h3>
+                <h3>Research Associates</h3>
+                <h3>Mission Statement</h3>
+            </div>
+            <div className="social-links">
+                <h3>Let's connect!</h3>
+                <a href="https://www.instagram.com/uoftampa/" target="_blank" rel="noopener noreferrer">
+                    <img src="/instagram.svg" alt="Instagram" />
+                </a>
+                <a href="https://x.com/UofTampa" target="_blank" rel="noopener noreferrer">
+                    <img src="/x.svg" alt="X" />
+                </a>
+                <a href="https://www.linkedin.com/company/utampa-criminology-criminal-justice/" target="_blank" rel="noopener noreferrer">
+                    <img src="/linkedin.svg" alt="Linkedin" />
+                </a>
+                <a href="https://www.tiktok.com/@uoftampa" target="_blank" rel="noopener noreferrer">
+                    <img src="/tik_tok.svg" alt="Tik Tok" />
+                </a>
             </div>
         </footer>
     );
