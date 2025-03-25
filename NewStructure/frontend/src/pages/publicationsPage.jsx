aec35c2c4c3a090b4dee6c390f839ea0b5f7a646
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2025-03-25 11:30:53 -0400

    Update publicationsPage.jsx

---
+++ NewStructure/frontend/src/pages/publicationsPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/navbar.jsx";
+// import ./publicationsPage.css
+
+
+function Publications() {
+
+
+    return (
+
+
+
+
+    )
+}
+
+export default Publications;
