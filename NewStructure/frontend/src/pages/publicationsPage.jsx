c3aa88e1ae2a18bb8e029d80218b626d0bea56e9
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2025-03-29 19:12:10 -0400

    basics of publications page

--- NewStructure/frontend/src/pages/publicationsPage.jsx
+++ NewStructure/frontend/src/pages/publicationsPage.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../components/navbar.jsx";
 import "./publicationsPage.css"
 
+function Publications({ role, email, name }) {
+    role = "publisher"
+    const [showUpload, setShowUpload] = useState(false);
+    //const [filteredPublications, setFilteredPublications] = useState(publications);
 
-function Publications(role, email, name) {
-    //role = "publisher"
+    function searchText() {
+        const searchInput = ""
+        const filter = ""
+        /*
+        if (filter === "title") {
+        }
+        else if (filter === "author") {
+        }
+        else if (filter === "keyword") {
+        }
+         */
+        setFilteredPublications(filteredPublications);
+    }
 
     return (
         <>
             {role === "publisher" && (
                 <div className="publisher-stuff">
-                <h2>My Publications</h2>
-                <h2>Under Review</h2>
+                    <h2>My Publications</h2>
+                    <button onClick={() => setShowUpload(true)}> Upload </button>
+                    {showUpload && (
+                        <div className="popup-overlay">
+                            <div className="upload-popup">
+                                <button onClick={() => setShowUpload(false)}>X</button>
+                                <h3>Test</h3>
+                            </div>
+                        </div>
+                    )}
+                    <h2>Under Review</h2>
                 </div>
-                )}
+            )}
 
             <h2>Publications</h2>
+            <div className="search-bar">
+                <input type="text" id="search" class="search-bar" onkeyup="searchText()" placeholder="Search..."/>
+                <select id="filter" className="filter-menu" onChange="searchText()">
+                    <option value="title">by Title</option>
+                    <option value="author">by Author</option>
+                    <option value="keyword">by Keyword</option>
+                </select>
+            </div>
+
         </>
-
-
-
-    )
+    );
 }
 
 export default Publications;
