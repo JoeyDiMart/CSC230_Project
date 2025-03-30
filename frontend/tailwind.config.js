413a098f9bb94b5a458fa14a2e1cb484231f2915
stamayoz <138267058+stamayoz@users.noreply.github.com>
2025-03-30 16:25:50 -0400

    added tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+
+  ],
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
