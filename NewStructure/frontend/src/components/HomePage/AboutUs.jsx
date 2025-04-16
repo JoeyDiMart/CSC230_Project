b7d4b945825a97e0388b6a826d763a0cd8cde760
stamayoz <138267058+stamayoz@users.noreply.github.com>
2025-04-15 23:38:42 -0400

    About Us Component Created

---
+++ NewStructure/frontend/src/components/HomePage/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AboutUs = () => {
+    return (
+        <section className="flex">
+            <div className="max-w-5xl mx-auto text-center">
+                <h2 className="flex justify-center text-3xl font-bold mb-4 text-testingColorBlack">About CIRT</h2>
+                <p className="flex justify-center text-center text-lg leading-relaxed text-testingColorGrey">
+                    The Criminology Institute for Research and Training (CIRT) at the University of Tampa was established in 2023 with a mission to drive innovation and excellence in the field of criminal justice. CIRT connects students, faculty, and professionals through high-impact research, interdisciplinary collaboration, and hands-on training opportunities.
+                </p>
+            </div>
+        </section>
+    );
+}
+export default AboutUs;
