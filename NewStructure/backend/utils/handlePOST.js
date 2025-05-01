c1ca0d3e0f8b97fe8fda5e2296d999faa5efd21d
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2025-05-01 10:11:47 -0400

    added import for pdf2p

--- NewStructure/backend/utils/handlePOST.js
+++ NewStructure/backend/utils/handlePOST.js
@@ -8,8 +8,14 @@ import multer from "multer";
 import fs from "fs";
 import { ObjectId } from 'mongodb';
 import * as crypto from 'crypto';
+<<<<<<< Updated upstream
 import * as userService from '../services/userService.js';
 import * as publicationService from '../services/publicationService.js';
+=======
+import { fromPath } from "pdf2pic";
+
+
+>>>>>>> Stashed changes
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
