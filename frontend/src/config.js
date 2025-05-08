d3cd10ea55db97c23fb827eaad45c1b7a536cfc2
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2025-05-08 12:35:26 -0400

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -7,8 +7,8 @@ const API_BASE_URL =
 export default API_BASE_URL;
 */
 
-const API_BASE_URL = "http://localhost:8081";
-//const API_BASE_URL = "https://cirtutampa-b47a791bfcb6.herokuapp.com";
+//const API_BASE_URL = "http://localhost:8081";
+const API_BASE_URL = "https://cirtutampa-b47a791bfcb6.herokuapp.com";
 //const API_BASE_URL = "https://csc-230-project-tau.vercel.app/";
 
 export default API_BASE_URL;
